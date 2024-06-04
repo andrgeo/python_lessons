a9f0f16df77dbd7d6c9bb4c98ea9605a4f806cc4
Georgiev Andrey <7175695@gmail.com>
2024-06-05 00:33:02 +0300

    complete task

---
+++ project2_2.py
@@ -0,0 +1,8 @@
+products = ["milk", "bread", "tomato", "cucumber", "peaper", "water", "tea", "shrimp", "brocolli"]
+price = [10, 15, 4, 2, 7, 3, 6, 7, 11, 16]
+work_id = [321, 467, 874, 984, 203, 384, 583, 219, 193, 738]
+
+orders = list(zip(products, price, work_id))
+
+print(orders)
+    
