efdf778488637d037da8c55d14173b98ca922362
Georgiev Andrey <7175695@gmail.com>
2024-06-20 22:01:35 +0300

    new commit

--- project/1_6B_v2.py
+++ project/1_6B_v2.py
@@ -19,44 +19,52 @@
 from pathlib import Path
 import datetime
 
-# Рабоччая директория
 current_folder = Path.cwd()
 my_folder = current_folder / "1_6B_files"
-# Дата
 cur_date = datetime.datetime.now().strftime("%d-%m-%Y_%H:%M")
-
-# Бэкап файл
 backup_file = str(cur_date) + "-backup.txt"
-bp_file = my_folder / backup_file
-bp_file.touch(exist_ok=True)
-# Лог файл
+(my_folder / backup_file).touch(exist_ok=True)
+
 f_for_log = my_folder / "files_copy.log"
 f_for_log.touch(exist_ok=True)
-# Список файлов
+
+
 files_list = ["new_orderdate.csv", "orderdata_sample.csv", "new.csv"]
 
 def read_file (file):
-    with open(file, 'r') as inp_file:
+    with open(my_folder / f, 'r') as inp_file:
         data = inp_file.read()
         return data
+
 def wrond_log(file, log_file):
-    with open(log_file, 'a') as f0:
-        f0.write(str(cur_date) + " " + "невыполнено копирование данных файла:" + " " + file + '\n')
+    with open(log_file, 'a') as log_file:
+        log_file.write(str(cur_date) + " " + "невыполнено копирование данных файла:" + " " + file + '\n')
+
 def right_log(file, log_file):
-    with open(log_file, 'a') as f1:
-        f1.write(str(cur_date) + " " + "выполнено копирование данных файла:" + " " + file + '\n')
+    with open(log_file, 'a') as log_file1:
+        log_file1.write(str(cur_date) + " " + "выполнено копирование данных файла:" + " " + file + '\n')
+
 def copy_data (data, file):
     with open(file, 'w') as back_file:
         back_file.write(data)
+
 
 for f in files_list:
     try:
         file = my_folder / f
         data = read_file(file)
+        '''with open(my_folder / f, 'r') as inp_file:
+            data = inp_file.read()'''
     except FileNotFoundError:
         print("Невозможно открыть файл", f)
         wrond_log(f, f_for_log)
+        '''with open(my_folder / "files_copy.log", 'a') as log_file:
+            log_file.write(str(cur_date) + " " + "невыполнено копирование данных файла:" + " " + f + '\n')'''
         continue
-    copy_data(data, bp_file)
-    right_log(f, f_for_log)
 
+    copy_data(data, file)
+    '''with open(my_folder / backup_file, 'w') as back_file:
+        back_file.write(data)'''
+    right_log(f, f_for_log)
+    '''with open(my_folder/ "files_copy.log", 'a') as log_file1:
+        log_file1.write(str(cur_date) + " " + "выполнено копирование данных файла:" + " " + f + '\n')'''
