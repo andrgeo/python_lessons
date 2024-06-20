62c78501031624b09a2ee93a022667dcea7de21b
Georgiev Andrey <7175695@gmail.com>
2024-06-20 20:51:43 +0300

    new commit

--- project/1_6B.py
+++ project/1_6B.py
@@ -18,16 +18,27 @@
 
 from pathlib import Path
 import datetime
-import csv
-import asposecells
-from asposecells.api import Workbook
 
-my_folder = Path.cwd()
+current_folder = Path.cwd()
+my_folder = current_folder / "1_6B_files"
+cur_date = datetime.datetime.now().strftime("%d-%m-%Y_%H:%M")
+backup_file = str(cur_date) + "-backup.txt"
+(my_folder / backup_file).touch(exist_ok=True)
+(my_folder / "files_copy.log").touch(exist_ok=True)
 
 files_list = ["new_orderdate.csv", "orderdata_sample.csv", "new.csv"]
-counter = 0
+
 for f in files_list:
-    workbook = Workbook(my_folder/f)
-    if workbook
+    try:
+        with open(my_folder / f, 'r') as inp_file:
+            data = inp_file.read()
+    except FileNotFoundError:
+        print("Невозможно открыть файл", f)
+        with open(my_folder / "files_copy.log", 'a') as log_file:
+            log_file.write(str(cur_date) + " " + "невыполнено копирование данных файла:" + " " + f + '\n')
+        continue
+    with open(my_folder / backup_file, 'w') as back_file:
+        back_file.write(data)
 
-
+    with open(my_folder/ "files_copy.log", 'a') as log_file1:
+        log_file1.write(str(cur_date) + " " + "выполнено копирование данных файла:" + " " + f + '\n')
